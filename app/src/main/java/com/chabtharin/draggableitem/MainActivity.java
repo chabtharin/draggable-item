e17d0f0dc5fe98c6cd55a4e4b030153d8034d96b
“Chab <chabtharin@gmail.com>
2022-10-06 20:02:16 +0700

    add change layout

--- app/src/main/java/com/chabtharin/draggableitem/MainActivity.java
+++ app/src/main/java/com/chabtharin/draggableitem/MainActivity.java
@@ -1,19 +1,25 @@
 package com.chabtharin.draggableitem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
 import com.chabtharin.draggableitem.adapter.DraggableAdapter;
 import com.chabtharin.draggableitem.databinding.ActivityMainBinding;
+import com.chabtharin.draggableitem.helpers.RecyclerItemTouchHelperListener;
+import com.chabtharin.draggableitem.helpers.RecyclerTouchHelper;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecyclerItemTouchHelperListener {
     private ActivityMainBinding binding;
     private List<String> mDataSet = new ArrayList<>();
+    private DraggableAdapter draggableAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +31,23 @@ public class MainActivity extends AppCompatActivity {
         mDataSet.add("Inventory");
         mDataSet.add("Masters");
         mDataSet.add("Users");
-        DraggableAdapter draggableAdapter = new DraggableAdapter(mDataSet);
+        draggableAdapter = new DraggableAdapter(mDataSet);
         binding.rcDraggable.setAdapter(draggableAdapter);
         binding.rcDraggable.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL));
+        ItemTouchHelper.SimpleCallback callback = new RecyclerTouchHelper(ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.RIGHT | ItemTouchHelper.END, 0, this);
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callback);
+        itemTouchHelper.attachToRecyclerView(binding.rcDraggable);
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+
+    }
+
+    @Override
+    public boolean onItemMove(int fromPosition, int toPosition) {
+        Collections.swap(mDataSet, fromPosition, toPosition);
+        draggableAdapter.notifyItemMoved(fromPosition, toPosition);
+        return true;
     }
 }
